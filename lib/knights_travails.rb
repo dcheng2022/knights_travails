e1766002c7f66a09654ed61ad41b1bb35c88daad
Darin <darinchengg@gmail.com>
2022-07-13 15:42:53 -0500

    Add class Tree with start, finish initializiations

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,5 +1,12 @@
 require 'pry-byebug'
 
+class Tree
+  def initialize(start, finish)
+    @root = start
+    @given_node = finish
+  end
+end
+
 def out_of_bounds?(coordinate)
   board = Array.new(8) { Array.new(8, 'x') }
   return false if board.dig(coordinate[0], coordinate[1])
@@ -10,5 +17,5 @@ end
 def find_moves(coordinate)
   x = coordinate[0]
   y = coordinate[1]
-  moves = [[x + 1, y + 2], [x + 1, y - 2], [x - 1, y + 2], [x - 1, y - 2], [y + 2, x + 1], [y + 2, x - 1], [y - 2, x + 1], [y - 2, x - 1]]
+  moves = [[x + 1, y + 2], [x + 1, y - 2], [x - 1, y + 2], [x - 1, y - 2], [x + 2, y + 1], [x + 2, y - 1], [x - 2, y + 1], [x - 2, y - 1]]
 end
