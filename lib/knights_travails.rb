2d80968f87c37cecc7bba31cc4bb710cf82a1a34
Darin <darinchengg@gmail.com>
2022-07-13 15:46:14 -0500

    Move #out_of_bounds? predicate method under Tree class

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,17 +1,19 @@
 require 'pry-byebug'
 
 class Tree
+  attr_reader :board
+
   def initialize(start, finish)
     @root = start
     @given_node = finish
+    @board = Array.new(8) { Array.new(8, 'x') }
   end
-end
 
-def out_of_bounds?(coordinate)
-  board = Array.new(8) { Array.new(8, 'x') }
-  return false if board.dig(coordinate[0], coordinate[1])
-
-  true
+  def out_of_bounds?(coordinate)
+    return false if board.dig(coordinate[0], coordinate[1])
+  
+    true
+  end
 end
 
 def find_moves(coordinate)
